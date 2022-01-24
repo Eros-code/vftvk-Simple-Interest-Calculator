170aa535ad9a12291a03f3a5cf01434307190445
Eros-code <Kreed17@hotmail.co.uk>
2022-01-24 11:57:15 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function highlight(text) {
-    var inputText = document.getElementById("inputText");
-    var innerHTML = inputText.innerHTML;
-    var index = innerHTML.indexOf(text);
-    if (index >= 0) { 
-     innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-     inputText.innerHTML = innerHTML;
-    }
-  }
-
 function compute()
 {
     var principal = document.getElementById("principal").value;
@@ -15,12 +5,20 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="\<br\>\<br\> If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>\<br\>"
+    if(principal == '') {
+        alert('Please enter a positive number for your amount');
+        document.getElementById("principal").focus();
+    } else if (principal <= 0) {
+        alert('Please enter a value above zero the calculator only works for positive values')
+        document.getElementById("principal").focus();
+    } else{
+        document.getElementById("result").innerHTML="\<br\>\<br\> If you deposit "+ "<span class='highlight'>" + principal + "</span>" + ",\<br\>at an interest rate of "+ "<span class='highlight'>" +rate+ "%</span>" + "\<br\>You will receive an amount of "+ "<span class='highlight'>" +interest+ "</span>" + ",\<br\>in the year " + "<span class='highlight'>" +year+ "</span>" + "\<br\>\<br\>";
 
 }
+};
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-}
+};
